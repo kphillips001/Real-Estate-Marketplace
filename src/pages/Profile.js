d429774e4cf9f33e6c2ea93ea487bb854515d5b6
Kevin Phillips <knphillips01@gmail.com>
2022-04-07 18:57:58 -0500

    now able to edit user details

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { updateDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase.config';
 import { useNavigate, Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function Profile() {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     // set date to what comes back from firebase
     name: auth.currentUser.displayName,
-    displayName: auth.currentUser.email,
+    email: auth.currentUser.email,
   });
+
+  const { name, email } = formData;
 
   const navigate = useNavigate();
 
@@ -17,14 +23,87 @@ function Profile() {
     navigate('/');
   };
 
+  // return a promise so need async
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update display name in fb
+        // takes in current user
+        // update display name to name in the form
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        // now need to update in firestore
+        // need to create a reference to update a document
+        // take in the db (takes in users) and id
+        const userRef = doc(db, 'users', auth.currentUser.uid);
+        // take in the reference just created and update the name field
+        await updateDoc(userRef, {
+          name,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Could not update profile details');
+    }
+  };
+
+  const onChange = (e) => {
+    // set formdate state
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <div className='profile'>
-      <header className='prifleHeader'>
+      <header className='profileHeader'>
         <p className='pageHeader'>My Profile</p>
-        <button type='logout' className='logOut' onClick={onLogout}>
+        <button type='button' className='logOut' onClick={onLogout}>
           Logout
         </button>
       </header>
+
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p
+            className='changePersonalDetails'
+            // Set to a function that says if change details(if true), then onClick
+            // to be in onSubmit or go to onSubmit function
+            // Set changes details to whatever the opposit is
+            // Pass in function  with previouis state and then set to opposite
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type='text'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
