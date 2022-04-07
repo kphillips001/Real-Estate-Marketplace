955f31bf10571e959ba77c873cd1446dfbf45f47
Kevin Phillips <knphillips01@gmail.com>
2022-04-06 22:09:58 -0500

    able to log in user

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,16 @@
+import { useState, useEffect } from 'react';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-    return (
-      <div>Profile</div>
-    )
-  }
-  export default Profile
+  const [user, setUser] = useState(null)
+
+
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+  
+  return user ? <h1>{user.displayName} </h1> : 'Not logged in'
+
+}
+export default Profile;
